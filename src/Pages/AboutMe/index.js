fdd7a12fc270da28a0cae78bef61c166ff56becd
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-07-27 22:10:52 -0300

    AboutMe

--- src/Pages/AboutMe/index.js
+++ src/Pages/AboutMe/index.js
@@ -12,21 +12,29 @@ const AboutMe = () => {
       <div className="about-me-wrapper">
         <img className="about-me-img" src={Eduardo} alt="Photo of Eduardo" />
         <div>
-            <p className="about-me-paragraph">
+          <p className="about-me-paragraph">
             {" "}
             I'm 21 years old and I was borned and raised in Rio Grande do Sul
             Brasil. When I was 10 years old my parents got me my first computer,
             back then I used it for playing games and making schoold projects,
-            with my first PC I got a passion for tecnologies, and that lead me to
-            a carrer as a software developer.{" "}
-            </p>
-            <p className="about-me-paragraph">
+            with my first PC I got a passion for tecnologies, and that lead me
+            to a carrer as a software developer.{" "}
+          </p>
+
+          <p className="about-me-paragraph">
             {" "}
-            Ever since I started my journey coding, I had a special conection with
-            Front-End, because it isn't only about the code, it's about making the
-            user feel something looking at your page. Front-end development for me
-            it's a form of art and expression.{" "}
-            </p>
+            Ever since I started my journey coding, I had a special conection
+            with Front-End, because it isn't only about the code, it's about
+            making the user feel something looking at your page. Front-end
+            development for me it's a form of art and expression.{" "}
+          </p>
+
+          <p className="about-me-paragraph">
+            {" "}
+            Currently I'm proficient in Front-End tecnologies such as HTML5,
+            CSS3, BootStrap, TailWind, JavaScript, React.js, JSON and APIs. For
+            the Back-End I've studied Python, Node.js and Next.js{" "}
+          </p>
         </div>
       </div>
     </PostModule>
