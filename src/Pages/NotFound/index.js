e35117d59f0ef1be025586a69642d38b6dd786b9
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-08-02 21:08:22 -0300

    404

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -6,6 +6,7 @@ const NotFound = () => {
         <div className='container404'>
             <h1 className='title404'>404</h1>
             <h2 className='subtitle404'>Page Not Found</h2>
+            
             <p className='text404'>Are you sure you it's this what you are looking for?</p>
             <Link to={'/'} className='button404'>Go back</Link>
         </div>
