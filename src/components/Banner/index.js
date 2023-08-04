55ec9373cd73c6e495f9bd2cce5a91ec2208af5c
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-08-04 14:46:55 -0300

    links

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -10,6 +10,14 @@ const Banner = () => {
                     Welcome to my personal space! I'm Eduardo Griesang, currently a Front-End Developer trying to migrate to 
                     a Full-Stack position. Here I will post about my day to day learning aswell as the knowledge I allready got.
                 </p>
+                <div className='button-presentation'>
+                    <a href='https://portfolio-eduardo-griesang.vercel.app' target="_blank">
+                        My Portfolio
+                    </a>
+                    <a href='https://www.linkedin.com/in/eduardo-griesang-0b779921b/' target="_blank">
+                        My LinkedIn
+                    </a>
+                </div>
             </div>
             <div className='images'>
                 <img src={Me} alt='Me' />
