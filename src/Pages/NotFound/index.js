e1f5c868c74c9f3206169f3a4519c46db0ce0937
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-08-07 22:42:31 -0300

    useNavigate

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,14 +1,21 @@
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import './NotFound.css'
 
 const NotFound = () => {
+
+    const navigate = useNavigate();
+    /* useNavigate passando (-1) funciona como o botão de voltar do navegador */
+
     return (
         <div className='container404'>
             <h1 className='title404'>404</h1>
             <h2 className='subtitle404'>Page Not Found</h2>
             
             <p className='text404'>Are you sure you it's this what you are looking for?</p>
-            <Link to={'/'} className='button404'>Go back</Link>
+
+            <button className='button404' onClick={() => navigate(-1)}>
+                Go back
+            </button>
         </div>
     )
 }
